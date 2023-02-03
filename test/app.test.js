ac5b5e4d2ad8972c4a963aa46eb75fc70280f85a
cd-vishal <vishal.chauhan@clickdee.com>
2023-02-03 20:03:36 +0530

    tesr

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const assert=require("chai").assert;
+const app=require("../index")
+
+describe('App',function(){
+    it('app should say hello', function(){
+        assert.equal(app(),'hello')
+    })
+    it('return value should be a string', function(){
+        assert.typeOf(app(), 'string')
+    })
+})
