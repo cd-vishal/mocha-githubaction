10d2d37d7257e08ed7a56f811f7b9e2a8a4f58d8
cd-vishal <vishal.chauhan@clickdee.com>
2023-02-03 20:15:46 +0530

    fixed

--- test/app.test.js
+++ test/app.test.js
@@ -3,9 +3,12 @@ const app=require("../index")
 
 describe('App',function(){
     it('app should say hello', function(){
-        assert.equal(app(),'hello')
+        assert.equal(app(),'racist')
     })
     it('return value should be a string', function(){
         assert.typeOf(app(), 'string')
+    })
+    it('length should be a 6', function(){
+        assert.lengthOf(app(),6)
     })
 })
